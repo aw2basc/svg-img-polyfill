39764945c12fc73768a5f9e9d053b6a75b87d9c8
Justin Maxwell <a@aw2basc.com>
2013-11-07 01:05:41 -0600

    svg img polyfill

--- svg-img-polyfill.js
+++ svg-img-polyfill.js
@@ -1,2 +1,2 @@
 // svg polyfill
-if(!(!!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect)) $('img').each(function(i,ele){ $(this).attr('src',($(this).attr('src')).replace(/\.svg/, '.png')); });
+if(!(!!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect)){$('img').each(function(){ $(this).attr('src',($(this).attr('src')).replace(/\.svg/, '.png')); });}
